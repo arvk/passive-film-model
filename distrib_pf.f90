6166b03e59d4a5be2bb33a4f8615c7e6a69d72ce
Aravind Krishnamoorthy <aravindk@mit.edu>
2014-12-11 11:27:43 -0500

    Added the pyrite orientation field to distrib_pf.f90

--- distrib_pf.f90
+++ distrib_pf.f90
@@ -8,20 +8,32 @@ subroutine distrib_pf()
   integer :: x, y, z   ! Loop variables
   integer :: ierr,status(MPI_STATUS_SIZE)   
 
+
+  !! Broadcast global phase-fraction fields
   call mpi_bcast(met_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(pht_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
-  call mpi_bcast(env_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(pyr_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
+  call mpi_bcast(env_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
+
+  !! Broadcast global chemical-potential fields
   call mpi_bcast(mu_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
+  !! Broadcast global pyrite orientation field
+  call mpi_bcast(opyr_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
+
+
+  !! Copy relevant portions of the global matrices onto local matrices
   do x = 1,psx
      do y = 1,psy
         do z = 2,psz+1
            met(x,y,z) = met_g(x,y,z-1+(rank*psz_g/procs))
            pht(x,y,z) = pht_g(x,y,z-1+(rank*psz_g/procs))
-           env(x,y,z) = env_g(x,y,z-1+(rank*psz_g/procs))
            pyr(x,y,z) = pyr_g(x,y,z-1+(rank*psz_g/procs))
+           env(x,y,z) = env_g(x,y,z-1+(rank*psz_g/procs))
+
            mu(x,y,z) = mu_g(x,y,z-1+(rank*psz_g/procs))
+
+           opyr(x,y,z) = opyr_g(x,y,z-1+(rank*psz_g/procs))
         end do
      end do
   end do
