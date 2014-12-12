bed3cab1a30b35ebd46629252bf4ce99af4e7b48
Aravind Krishnamoorthy <aravindk@mit.edu>
2014-12-12 12:55:08 -0500

    Added calc_grad_pf.f90 that computes the gradient of the pyrite phase field. Useful in including anisotropic interfacial energies.

---
+++ calc_grad_pf.f90
@@ -0,0 +1,45 @@
+subroutine calc_grad_pf
+  use commondata
+  use fields
+  use gradients
+  implicit none
+
+  integer :: x, y, z   ! Loop variables
+  integer :: wrap
+
+!!!! Initialize all variables to zero
+  delypyr = 0.0d0 
+
+!!!!! Calculating laplacian in the bulk
+
+  do x = 1,psx
+     do y = 1,psy
+        do z = 2,psz+1
+
+           delypyr(x,y,z) = (pyr(x,wrap(y+1,psy),z)-pyr(x,wrap(y-1,psy),z))/(2*dpf)
+           delzpyr(x,y,z) = (pyr(x,y,z+1)-pyr(x,y,z-1))/(2*dpf)
+
+        end do
+     end do
+  end do
+
+
+!!! Impose boundary conditions on the Laplacian
+
+  do x = 1,psx
+     do y = 1,psy
+        delypyr(x,y,1) = 0.0d0 ; delypyr(x,y,psz+2) = 0.0d0
+     end do
+  end do
+
+end subroutine calc_grad_pf
+
+
+
+
+integer function wrap(a,lim)
+  implicit none
+  integer, intent(in) :: a,lim
+  wrap = modulo(a-1,lim)+1
+  return
+end function wrap
