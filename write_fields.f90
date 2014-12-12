ff804a8d11dd1c700be55dfdb70f2b7a310b2fa0
Aravind Krishnamoorthy <aravindk@mit.edu>
2014-12-11 22:50:55 -0500

    write_fields.f90 now also outputs the pyrite orientation field.

--- write_fields.f90
+++ write_fields.f90
@@ -8,82 +8,94 @@ subroutine write_fields(iter)
   integer :: x, y, z
   character*5 :: img_id
 
-  call system("rm -rf PHT.out ENV.out MET.out PYR.out MUS.out Hplus.out ALL.out")
+  call system("rm -rf PHT.out ENV.out MET.out PYR.out MUS.out OPYR.out")
 
-  open (unit=101, file="PHT.out", status="new")
+
+  !! Write phase fraction fields
+  open (unit=101, file="MET.out", status="new")
   write(101,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
   do x = 1,psx_g
      do y = 1,psy_g
         do z = 1,psz_g
-           write(101,*)  pht_g(x,y,z)
+           write(101,*)  met_g(x,y,z)
         end do
      end do
   end do
   close(101)  
 
-  open (unit=102, file="ENV.out", status="new")
+  open (unit=102, file="PHT.out", status="new")
   write(102,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
   do x = 1,psx_g
      do y = 1,psy_g
         do z = 1,psz_g
-           write(102,*)  env_g(x,y,z)
+           write(102,*)  pht_g(x,y,z)
         end do
      end do
   end do
   close(102)  
 
-  open (unit=103, file="MET.out", status="new")
+  open (unit=103, file="PYR.out", status="new")
   write(103,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
-  do x = 1,psx_g
+  do x = 1,psx_g 
      do y = 1,psy_g
         do z = 1,psz_g
-           write(103,*)  met_g(x,y,z)
+           write(103,*)  pyr_g(x,y,z)
         end do
      end do
   end do
   close(103)  
 
-  open (unit=105, file="MUS.out", status="new")
-  write(105,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
-  do x = 1,psx_g 
+  open (unit=104, file="ENV.out", status="new")
+  write(104,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
+  do x = 1,psx_g
      do y = 1,psy_g
         do z = 1,psz_g
-           write(105,*)  mu_g(x,y,z)
+           write(104,*)  env_g(x,y,z)
         end do
      end do
   end do
-  close(105)  
-
-!   open (unit=107, file="ALL.out", status="new")
-!   write(107,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
-!   do x = 1,psx_g 
-!      do y = 1,psy_g
-!         do z = 1,psz_g
-!            write(107,*)  (met_g(x,y,z)*1.0d0)+(pht_g(x,y,z)*2.0d0)+(env_g(x,y,z)*4.0d0)
-!         end do
-!      end do
-!   end do
-!   close(107)  
+  close(104)  
 
 
-  open (unit=108, file="PYR.out", status="new")
-  write(108,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
+
+  !! Write chemical potential fields
+  open (unit=200, file="MUS.out", status="new")
+  write(200,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
   do x = 1,psx_g 
      do y = 1,psy_g
         do z = 1,psz_g
-           write(108,*)  pyr_g(x,y,z)
+           write(200,*)  mu_g(x,y,z)
         end do
      end do
   end do
-  close(108)  
+  close(200)  
+
+
+
+  !! Write orientation fields
+  open (unit=304, file="OPYR.out", status="new")
+  write(304,*) "#", psx_g, psy_g, psz_g, ksx_g, ksy_g
+  do x = 1,psx_g 
+     do y = 1,psy_g
+        do z = 1,psz_g
+           write(304,*)  opyr_g(x,y,z)
+        end do
+     end do
+  end do
+  close(304)  
+
+
+
 
   write(img_id,'(I5.5)') iter/((nomc/noimg)-1)
-  call system("cp PHT.out PHT_"//img_id//".out")
-  call system("cp ENV.out ENV_"//img_id//".out")
   call system("cp MET.out MET_"//img_id//".out")
+  call system("cp PHT.out PHT_"//img_id//".out")
   call system("cp PYR.out PYR_"//img_id//".out")
+  call system("cp ENV.out ENV_"//img_id//".out")
+
   call system("cp MUS.out MUS_"//img_id//".out")
-!  call system("cp ALL.out ALL_"//img_id//".out")
+
+  call system("cp OPYR.out OPYR_"//img_id//".out")
 
 end subroutine write_fields
 
