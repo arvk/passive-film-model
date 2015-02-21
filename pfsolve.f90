d4e2d3548f1b9b76a4168922b14c78b09db16ea4
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-02-21 18:03:43 -0500

    Mackinawite phase fields are solved-for and time-stepped

--- pfsolve.f90
+++ pfsolve.f90
@@ -15,24 +15,18 @@ subroutine pfsolve(iter)
   !!---------------PF evolution-----------------!!
 
   !! Bulk free energy 
-  real*8 :: f_pht, f_env, f_met, f_pyr
-  real*8 :: w_pht, w_env, w_met, w_pyr
+  real*8 :: f_met, f_mkw, f_pht, f_pyr, f_env
+  real*8 :: w_met, w_mkw, w_pht, w_pyr, w_env
 
   !! Derivative of bulk free energy with phase field
-  real*8 :: dF_dpht_met, dF_dpht_env, dF_dpht_pyr
-  real*8 :: dF_dmet_pht, dF_dmet_env, dF_dmet_pyr
-  real*8 :: dF_denv_met, dF_denv_pht, dF_denv_pyr
-  real*8 :: dF_dpyr_met, dF_dpyr_pht, dF_dpyr_env
+  real*8 :: dF_dmet_mkw, dF_dmet_pht, dF_dmet_pyr, dF_dmet_env
+  real*8 :: dF_dmkw_met, dF_dmkw_pht, dF_dmkw_pyr, dF_dmkw_env
+  real*8 :: dF_dpht_met, dF_dpht_mkw, dF_dpht_pyr, dF_dpht_env
+  real*8 :: dF_dpyr_met, dF_dpyr_mkw, dF_dpyr_pht, dF_dpyr_env
+  real*8 :: dF_denv_met, dF_denv_mkw, dF_denv_pht, dF_denv_pyr
 
-
-
-  real*8 :: sigma_pht_pyr
-  real*8 :: sigma_pyr_pht
-  real*8 :: sigma_met_pyr
-  real*8 :: sigma_pyr_met
-  real*8 :: sigma_env_pyr
-  real*8 :: sigma_pyr_env
-
+  real*8 :: sigma_pyr_met, sigma_pyr_mkw, sigma_pyr_pht, sigma_pyr_env
+  real*8 :: sigma_met_pyr, sigma_mkw_pyr, sigma_pht_pyr, sigma_env_pyr
 
   integer :: int_count
 
@@ -44,7 +38,7 @@ subroutine pfsolve(iter)
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   !! Initialize field variables to 0
-  dpht_dt = 0.0d0 ; denv_dt = 0.0d0 ; dmet_dt = 0.0d0 ; dpyr_dt = 0.0d0 ; dmu_dt = 0.0d0 
+ dmet_dt = 0.0d0 ; dmkw_dt = 0.0d0 ; dpht_dt = 0.0d0 ; dpyr_dt = 0.0d0 ; denv_dt = 0.0d0
 
      if (mod(iter,swap_freq_pf).eq.1) then
         call swap_pf()
@@ -80,11 +74,25 @@ subroutine pfsolve(iter)
      do y = 1,psy
         do z = 2,psz+1
 
+           !! Volume per mole of Fe = 7.122 cc assuming a density of 7.84 g/cc
+           !! Number of moles per m^3 = 140401
+           f_met = 0.0d0*140401
+           w_met = 0.0d0*140401  ! f_met - (mu(x,y,z)*140401*0.0015d0)
+
+           !! Volume per mole of Mkw = 19.130 cc assuming a density of 4.28 g/cc from Lennie et. al. Mineralogical Magazine, December, Vol. 59, pp. 677-683
+           !! Number of moles per m^3 = 48683
+           f_mkw = ((mu(x,y,z)*mu(x,y,z)*(1E-6)) + 20.53*T - 70060)*48683.0d0
+           w_mkw = f_pht - (mu(x,y,z)*48683.0d0*0.95d0)  ! 0.95 because mackinawite is sligtly iron-rich and sulfur deficient
+
            !! Volume per mole of Pht = 19.130 cc assuming a density of 4.6 g/cc
            !! Number of moles per m^3 = 52275
            f_pht = ((mu(x,y,z)*mu(x,y,z)*(1E-6)) + 20.53*T - 72050)*52275
            w_pht = f_pht - (mu(x,y,z)*52275)
 
+           !! Volumer per mole of Pyrite = 24 cc assuming a density of 5 g/cc
+           !! Number of moles per m^3 = 41667
+           f_pyr = ((mu(x,y,z)*mu(x,y,z)*(1E-9)) + 50.355*T - 98710)*41667
+           w_pyr = f_pyr - (mu(x,y,z)*2*41667)
 
            !! Volume per mole of air = 22.4 * (T/298) * 1E3 cc
            !! Number of moles per m^3 = 13303/T
@@ -92,60 +100,25 @@ subroutine pfsolve(iter)
            w_env = 0.0d0
 
 
-           !! Volume per mole of Fe = 7.122 cc assuming a density of 7.84 g/cc
-           !! Number of moles per m^3 = 140401
-           f_met = 0.0d0*140401
-
-           ! if (exp(mu(x,y,z)/(R*T)) .lt. 0.0015d0) then
-           !    w_met = f_met - (mu(x,y,z)*exp(mu(x,y,z)/(R*T))*140401)
-           ! else
-           !    w_met = f_met - (mu(x,y,z)*140401*0.0015d0)
-           ! end if
-
-!           w_met = f_met - (mu(x,y,z)*140401*0.0015d0)
-           w_met = 0.0d0*140401
-
-
-           !! Volumer per mole of Pyrite = 24 cc assuming a density of 5 g/cc
-           !! Number of moles per m^3 = 41667
-           f_pyr = ((mu(x,y,z)*mu(x,y,z)*(1E-9)) + 50.355*T - 98710)*41667
-           w_pyr = f_pyr - (mu(x,y,z)*2*41667)
-
 
            sigma_pyr_met = sigma_pyr_met_0*(1+0.5*cos(4*(0.185+atan(delypyr(x,y,z)/(delzpyr(x,y,z)+1E-14)))-opyr(x,y,z)))
+           sigma_pyr_mkw = sigma_pyr_mkw_0*(1+0.5*cos(4*(0.185+atan(delypyr(x,y,z)/(delzpyr(x,y,z)+1E-14)))-opyr(x,y,z)))
            sigma_pyr_pht = sigma_pyr_pht_0*(1+0.5*cos(4*(0.185+atan(delypyr(x,y,z)/(delzpyr(x,y,z)+1E-14)))-opyr(x,y,z)))
            sigma_pyr_env = sigma_pyr_env_0*(1+0.5*cos(4*(0.185+atan(delypyr(x,y,z)/(delzpyr(x,y,z)+1E-14)))-opyr(x,y,z)))
 
            sigma_met_pyr = sigma_pyr_met
+           sigma_mkw_pyr = sigma_pyr_mkw
            sigma_pht_pyr = sigma_pyr_pht
            sigma_env_pyr = sigma_pyr_env
 
 
-           dF_dpht_met = (sigma_pht_met*((pht(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2pht(x,y,z)))) + &
-                & (2*hill_pht_met*pht(x,y,z)*met(x,y,z)*(pht(x,y,z)-met(x,y,z))) - &
-                & (2*(pht(x,y,z)+met(x,y,z))*(pht(x,y,z)+met(x,y,z))*(w_pht-w_met))
-
-           dF_dpht_env = (sigma_pht_env*((pht(x,y,z)*del2env(x,y,z))-(env(x,y,z)*del2pht(x,y,z)))) + &
-                & (2*hill_pht_env*pht(x,y,z)*env(x,y,z)*(pht(x,y,z)-env(x,y,z))) - &
-                & (2*(pht(x,y,z)+env(x,y,z))*(pht(x,y,z)+env(x,y,z))*(w_pht-w_env))
-
-           dF_dpht_pyr = (sigma_pht_pyr*((pht(x,y,z)*del2pyr(x,y,z))-(pyr(x,y,z)*del2pht(x,y,z)))) + &
-                & (2*hill_pht_pyr*pht(x,y,z)*pyr(x,y,z)*(pht(x,y,z)-pyr(x,y,z))) - &
-                & (2*(pht(x,y,z)+pyr(x,y,z))*(pht(x,y,z)+pyr(x,y,z))*(w_pht-w_pyr))
 
 
-           dF_denv_met = (sigma_env_met*((env(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2env(x,y,z)))) + &
-                & (2*hill_env_met*env(x,y,z)*met(x,y,z)*(env(x,y,z)-met(x,y,z))) - &
-                & (2*(env(x,y,z)+met(x,y,z))*(env(x,y,z)+met(x,y,z))*(w_env-w_met))
+!~~~~~~~~MET~~~~~~~~!
 
-           dF_denv_pht = (sigma_env_pht*((env(x,y,z)*del2pht(x,y,z))-(pht(x,y,z)*del2env(x,y,z)))) + &
-                & (2*hill_env_pht*env(x,y,z)*pht(x,y,z)*(env(x,y,z)-pht(x,y,z))) - &
-                & (2*(env(x,y,z)+pht(x,y,z))*(env(x,y,z)+pht(x,y,z))*(w_env-w_pht))
-
-           dF_denv_pyr = (sigma_env_pyr*((env(x,y,z)*del2pyr(x,y,z))-(pyr(x,y,z)*del2env(x,y,z)))) + &
-                & (2*hill_env_pyr*env(x,y,z)*pyr(x,y,z)*(env(x,y,z)-pyr(x,y,z))) - &
-                & (2*(env(x,y,z)+pyr(x,y,z))*(env(x,y,z)+pyr(x,y,z))*(w_env-w_pyr))
-
+           dF_dmet_mkw = (sigma_met_mkw*((met(x,y,z)*del2mkw(x,y,z))-(mkw(x,y,z)*del2met(x,y,z)))) + &
+                & (2*hill_met_mkw*met(x,y,z)*mkw(x,y,z)*(met(x,y,z)-mkw(x,y,z))) - &
+                & (2*(met(x,y,z)+mkw(x,y,z))*(met(x,y,z)+mkw(x,y,z))*(w_met-w_mkw))
 
            dF_dmet_pht = (sigma_met_pht*((met(x,y,z)*del2pht(x,y,z))-(pht(x,y,z)*del2met(x,y,z)))) + &
                 & (2*hill_met_pht*met(x,y,z)*pht(x,y,z)*(met(x,y,z)-pht(x,y,z))) - &
@@ -159,10 +132,51 @@ subroutine pfsolve(iter)
                 & (2*hill_met_pyr*met(x,y,z)*pyr(x,y,z)*(met(x,y,z)-pyr(x,y,z))) - &
                 & (2*(met(x,y,z)+pyr(x,y,z))*(met(x,y,z)+pyr(x,y,z))*(w_met-w_pyr))
 
+!~~~~~~~~MKW~~~~~~~~!
+
+           dF_dmkw_met = (sigma_mkw_met*((mkw(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2mkw(x,y,z)))) + &
+                & (2*hill_mkw_met*mkw(x,y,z)*met(x,y,z)*(mkw(x,y,z)-met(x,y,z))) - &
+                & (2*(mkw(x,y,z)+met(x,y,z))*(mkw(x,y,z)+met(x,y,z))*(w_mkw-w_met))
+
+           dF_dmkw_pht = (sigma_mkw_pht*((mkw(x,y,z)*del2pht(x,y,z))-(pht(x,y,z)*del2mkw(x,y,z)))) + &
+                & (2*hill_mkw_pht*mkw(x,y,z)*pht(x,y,z)*(mkw(x,y,z)-pht(x,y,z))) - &
+                & (2*(mkw(x,y,z)+pht(x,y,z))*(mkw(x,y,z)+pht(x,y,z))*(w_mkw-w_pht))
+
+           dF_dmkw_env = (sigma_mkw_env*((mkw(x,y,z)*del2env(x,y,z))-(env(x,y,z)*del2mkw(x,y,z)))) + &
+                & (2*hill_mkw_env*mkw(x,y,z)*env(x,y,z)*(mkw(x,y,z)-env(x,y,z))) - &
+                & (2*(mkw(x,y,z)+env(x,y,z))*(mkw(x,y,z)+env(x,y,z))*(w_mkw-w_env))
+
+           dF_dmkw_pyr = (sigma_mkw_pyr*((mkw(x,y,z)*del2pyr(x,y,z))-(pyr(x,y,z)*del2mkw(x,y,z)))) + &
+                & (2*hill_mkw_pyr*mkw(x,y,z)*pyr(x,y,z)*(mkw(x,y,z)-pyr(x,y,z))) - &
+                & (2*(mkw(x,y,z)+pyr(x,y,z))*(mkw(x,y,z)+pyr(x,y,z))*(w_mkw-w_pyr))
+
+!~~~~~~~~PHT~~~~~~~~!
+
+           dF_dpht_met = (sigma_pht_met*((pht(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2pht(x,y,z)))) + &
+                & (2*hill_pht_met*pht(x,y,z)*met(x,y,z)*(pht(x,y,z)-met(x,y,z))) - &
+                & (2*(pht(x,y,z)+met(x,y,z))*(pht(x,y,z)+met(x,y,z))*(w_pht-w_met))
+
+           dF_dpht_mkw = (sigma_pht_mkw*((pht(x,y,z)*del2mkw(x,y,z))-(mkw(x,y,z)*del2pht(x,y,z)))) + &
+                & (2*hill_pht_mkw*pht(x,y,z)*mkw(x,y,z)*(pht(x,y,z)-mkw(x,y,z))) - &
+                & (2*(pht(x,y,z)+mkw(x,y,z))*(pht(x,y,z)+mkw(x,y,z))*(w_pht-w_mkw))
+
+           dF_dpht_env = (sigma_pht_env*((pht(x,y,z)*del2env(x,y,z))-(env(x,y,z)*del2pht(x,y,z)))) + &
+                & (2*hill_pht_env*pht(x,y,z)*env(x,y,z)*(pht(x,y,z)-env(x,y,z))) - &
+                & (2*(pht(x,y,z)+env(x,y,z))*(pht(x,y,z)+env(x,y,z))*(w_pht-w_env))
+
+           dF_dpht_pyr = (sigma_pht_pyr*((pht(x,y,z)*del2pyr(x,y,z))-(pyr(x,y,z)*del2pht(x,y,z)))) + &
+                & (2*hill_pht_pyr*pht(x,y,z)*pyr(x,y,z)*(pht(x,y,z)-pyr(x,y,z))) - &
+                & (2*(pht(x,y,z)+pyr(x,y,z))*(pht(x,y,z)+pyr(x,y,z))*(w_pht-w_pyr))
+
+!~~~~~~~~PYR~~~~~~~~!
 
            dF_dpyr_met = (sigma_pyr_met*((pyr(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2pyr(x,y,z)))) + &
                 & (2*hill_pyr_met*pyr(x,y,z)*met(x,y,z)*(pyr(x,y,z)-met(x,y,z))) - &
                 & (2*(pyr(x,y,z)+met(x,y,z))*(pyr(x,y,z)+met(x,y,z))*(w_pyr-w_met))
+
+           dF_dpyr_mkw = (sigma_pyr_mkw*((pyr(x,y,z)*del2mkw(x,y,z))-(mkw(x,y,z)*del2pyr(x,y,z)))) + &
+                & (2*hill_pyr_mkw*pyr(x,y,z)*mkw(x,y,z)*(pyr(x,y,z)-mkw(x,y,z))) - &
+                & (2*(pyr(x,y,z)+mkw(x,y,z))*(pyr(x,y,z)+mkw(x,y,z))*(w_pyr-w_mkw))
 
            dF_dpyr_pht = (sigma_pyr_pht*((pyr(x,y,z)*del2pht(x,y,z))-(pht(x,y,z)*del2pyr(x,y,z)))) + &
                 & (2*hill_pyr_pht*pyr(x,y,z)*pht(x,y,z)*(pyr(x,y,z)-pht(x,y,z))) - &
@@ -172,32 +186,80 @@ subroutine pfsolve(iter)
                 & (2*hill_pyr_env*pyr(x,y,z)*env(x,y,z)*(pyr(x,y,z)-env(x,y,z))) - &
                 & (2*(pyr(x,y,z)+env(x,y,z))*(pyr(x,y,z)+env(x,y,z))*(w_pyr-w_env))
 
+!~~~~~~~~PYR~~~~~~~~!
+
+           dF_denv_met = (sigma_env_met*((env(x,y,z)*del2met(x,y,z))-(met(x,y,z)*del2env(x,y,z)))) + &
+                & (2*hill_env_met*env(x,y,z)*met(x,y,z)*(env(x,y,z)-met(x,y,z))) - &
+                & (2*(env(x,y,z)+met(x,y,z))*(env(x,y,z)+met(x,y,z))*(w_env-w_met))
+
+           dF_denv_mkw = (sigma_env_mkw*((env(x,y,z)*del2mkw(x,y,z))-(mkw(x,y,z)*del2env(x,y,z)))) + &
+                & (2*hill_env_mkw*env(x,y,z)*mkw(x,y,z)*(env(x,y,z)-mkw(x,y,z))) - &
+                & (2*(env(x,y,z)+mkw(x,y,z))*(env(x,y,z)+mkw(x,y,z))*(w_env-w_mkw))
+
+           dF_denv_pht = (sigma_env_pht*((env(x,y,z)*del2pht(x,y,z))-(pht(x,y,z)*del2env(x,y,z)))) + &
+                & (2*hill_env_pht*env(x,y,z)*pht(x,y,z)*(env(x,y,z)-pht(x,y,z))) - &
+                & (2*(env(x,y,z)+pht(x,y,z))*(env(x,y,z)+pht(x,y,z))*(w_env-w_pht))
+
+           dF_denv_pyr = (sigma_env_pyr*((env(x,y,z)*del2pyr(x,y,z))-(pyr(x,y,z)*del2env(x,y,z)))) + &
+                & (2*hill_env_pyr*env(x,y,z)*pyr(x,y,z)*(env(x,y,z)-pyr(x,y,z))) - &
+                & (2*(env(x,y,z)+pyr(x,y,z))*(env(x,y,z)+pyr(x,y,z))*(w_env-w_pyr))
+
+
+
+
+
+
+
+
+
+
+
+
 
            !! Error correction        
-           correct_rounding = 0.50d0*(dF_dpht_met - dF_dmet_pht)
-           dF_dpht_met = correct_rounding ; dF_dmet_pht = 0.0d0 - correct_rounding
+           correct_rounding = 0.50d0*(dF_dmet_mkw - dF_dmkw_met)
+           dF_dmet_mkw = correct_rounding ; dF_dmkw_met = 0.0d0 - correct_rounding
 
-           correct_rounding = 0.50d0*(dF_dpht_env - dF_denv_pht)
-           dF_dpht_env = correct_rounding ; dF_denv_pht = 0.0d0 - correct_rounding
-
-           correct_rounding = 0.50d0*(dF_dpht_pyr - dF_dpyr_pht)
-           dF_dpht_pyr = correct_rounding ; dF_dpyr_pht = 0.0d0 - correct_rounding
-
-           correct_rounding = 0.50d0*(dF_denv_met - dF_dmet_env)
-           dF_denv_met = correct_rounding ; dF_dmet_env = 0.0d0 - correct_rounding
-
-           correct_rounding = 0.50d0*(dF_denv_pyr - dF_dpyr_env)
-           dF_denv_pyr = correct_rounding ; dF_dpyr_env = 0.0d0 - correct_rounding
+           correct_rounding = 0.50d0*(dF_dmet_pht - dF_dpht_met)
+           dF_dmet_pht = correct_rounding ; dF_dpht_met = 0.0d0 - correct_rounding
 
            correct_rounding = 0.50d0*(dF_dmet_pyr - dF_dpyr_met)
            dF_dmet_pyr = correct_rounding ; dF_dpyr_met = 0.0d0 - correct_rounding
 
+           correct_rounding = 0.50d0*(dF_dmet_env - dF_denv_met)
+           dF_dmet_env = correct_rounding ; dF_denv_met = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dmkw_pht - dF_dpht_mkw)
+           dF_dmkw_pht = correct_rounding ; dF_dpht_mkw = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dmkw_pyr - dF_dpyr_mkw)
+           dF_dmkw_pyr = correct_rounding ; dF_dpyr_mkw = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dmkw_env - dF_denv_mkw)
+           dF_dmkw_env = correct_rounding ; dF_denv_mkw = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dpht_pyr - dF_dpyr_pht)
+           dF_dpht_pyr = correct_rounding ; dF_dpyr_pht = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dpht_env - dF_denv_pht)
+           dF_dpht_env = correct_rounding ; dF_denv_pht = 0.0d0 - correct_rounding
+
+           correct_rounding = 0.50d0*(dF_dpyr_env - dF_denv_pyr)
+           dF_dpyr_env = correct_rounding ; dF_denv_pyr = 0.0d0 - correct_rounding
+
+
+
+
+
+
+
            !! Calculate phase field(s) update
-           dpht_dt(x,y,z) = (M_pht_met*dF_dpht_met)+(M_pht_env*dF_dpht_env)+(M_pht_pyr*dF_dpht_pyr)
-           denv_dt(x,y,z) = (M_env_met*dF_denv_met)+(M_env_pht*dF_denv_pht)+(M_env_pyr*dF_denv_pyr)
-           dmet_dt(x,y,z) = (M_met_pht*dF_dmet_pht)+(M_met_env*dF_dmet_env)+(M_met_pyr*dF_dmet_pyr)
-           dpyr_dt(x,y,z) = (M_pyr_pht*dF_dpyr_pht)+(M_pyr_env*dF_dpyr_env)+(M_pyr_met*dF_dpyr_met)
-           dpyr_dt(x,y,z) = dpyr_dt(x,y,z) - (25.0d0*pyr(x,y,z)*((M_pyr_pht+M_pyr_pht+M_pyr_pht)*del_opyr(x,y,z)))
+           dmet_dt(x,y,z) = (M_met_mkw*dF_dmet_mkw)+(M_met_pht*dF_dmet_pht)+(M_met_pyr*dF_dmet_pyr)+(M_met_env*dF_dmet_env)
+           dmkw_dt(x,y,z) = (M_mkw_met*dF_dmkw_met)+(M_mkw_pht*dF_dmkw_pht)+(M_mkw_pyr*dF_dmkw_pyr)+(M_mkw_env*dF_dmkw_env)
+           dpht_dt(x,y,z) = (M_pht_met*dF_dpht_met)+(M_pht_mkw*dF_dpht_mkw)+(M_pht_pyr*dF_dpht_pyr)+(M_pht_env*dF_dpht_env)
+           dpyr_dt(x,y,z) = (M_pyr_met*dF_dpyr_met)+(M_pyr_mkw*dF_dpyr_mkw)+(M_pyr_pht*dF_dpyr_pht)+(M_pyr_env*dF_dpyr_env)
+           denv_dt(x,y,z) = (M_env_met*dF_denv_met)+(M_env_mkw*dF_denv_mkw)+(M_env_pht*dF_denv_pht)+(M_env_pyr*dF_denv_pyr)
+           dpyr_dt(x,y,z) = dpyr_dt(x,y,z) - (25.0d0*pyr(x,y,z)*((M_pyr_met+M_pyr_mkw+M_pyr_pht+M_pyr_env)*del_opyr(x,y,z)))
         end do
      end do
   end do
@@ -208,19 +270,21 @@ subroutine pfsolve(iter)
   if (rank.eq.0) then
      do x = 1,psx
         do y = 1,psy
-           dpht_dt(x,y,2) = 0.0d0
-           denv_dt(x,y,2) = 0.0d0
            dmet_dt(x,y,2) = 0.0d0
+           dmkw_dt(x,y,2) = 0.0d0
+           dpht_dt(x,y,2) = 0.0d0
            dpyr_dt(x,y,2) = 0.0d0
+           denv_dt(x,y,2) = 0.0d0
         end do
      end do
   elseif(rank.eq.procs-1) then
      do x = 1,psx
         do y = 1,psy
-           dpht_dt(x,y,psz+1) = 0.0d0
-           denv_dt(x,y,psz+1) = 0.0d0
            dmet_dt(x,y,psz+1) = 0.0d0
+           dmkw_dt(x,y,psz+1) = 0.0d0
+           dpht_dt(x,y,psz+1) = 0.0d0
            dpyr_dt(x,y,psz+1) = 0.0d0
+           denv_dt(x,y,psz+1) = 0.0d0
         end do
      end do
   end if
@@ -231,14 +295,18 @@ subroutine pfsolve(iter)
      do y = 1,psy
         do z = 2,psz+1
            newmet(x,y,z) = max(min((met(x,y,z) + (dt*dmet_dt(x,y,z))),1.0d0),0.0d0)
+           newmkw(x,y,z) = max(min((mkw(x,y,z) + (dt*dmkw_dt(x,y,z))),1.0d0),0.0d0)
            newpht(x,y,z) = max(min((pht(x,y,z) + (dt*dpht_dt(x,y,z))),1.0d0),0.0d0)
            newenv(x,y,z) = max(min((env(x,y,z) + (dt*denv_dt(x,y,z))),1.0d0),0.0d0)
            newpyr(x,y,z) = max(min((pyr(x,y,z) + (dt*dpyr_dt(x,y,z))),1.0d0),0.0d0)
 
-           newmet(x,y,z) = newmet(x,y,z)/(newmet(x,y,z)+newpht(x,y,z)+newenv(x,y,z)+newpyr(x,y,z))
-           newpht(x,y,z) = newpht(x,y,z)/(newmet(x,y,z)+newpht(x,y,z)+newenv(x,y,z)+newpyr(x,y,z))
-           newenv(x,y,z) = newenv(x,y,z)/(newmet(x,y,z)+newpht(x,y,z)+newenv(x,y,z)+newpyr(x,y,z))
-           newpyr(x,y,z) = newpyr(x,y,z)/(newmet(x,y,z)+newpht(x,y,z)+newenv(x,y,z)+newpyr(x,y,z))
+           correct_rounding = (newmet(x,y,z)+newmkw(x,y,z)+newpht(x,y,z)+newenv(x,y,z)+newpyr(x,y,z))
+
+           newmet(x,y,z) = newmet(x,y,z)/correct_rounding
+           newmkw(x,y,z) = newmkw(x,y,z)/correct_rounding
+           newpht(x,y,z) = newpht(x,y,z)/correct_rounding
+           newenv(x,y,z) = newenv(x,y,z)/correct_rounding
+           newpyr(x,y,z) = newpyr(x,y,z)/correct_rounding
         end do
      end do
   end do
@@ -248,10 +316,11 @@ subroutine pfsolve(iter)
      do y = 1,psy
         do z = 2,psz+1
            if (env(x,y,z).gt.0.99) then
-              newenv(x,y,z) = 1.0d0
               newmet(x,y,z) = 0.0d0
+              newmkw(x,y,z) = 0.0d0
               newpht(x,y,z) = 0.0d0
               newpyr(x,y,z) = 0.0d0
+              newenv(x,y,z) = 1.0d0
            end if
         end do
      end do
@@ -261,10 +330,11 @@ subroutine pfsolve(iter)
   do x = 1,psx
      do y = 1,psy
         do z = 2,psz+1
-           dpht_dt(x,y,z) = (newpht(x,y,z) - pht(x,y,z))/dt
-           denv_dt(x,y,z) = (newenv(x,y,z) - env(x,y,z))/dt
            dmet_dt(x,y,z) = (newmet(x,y,z) - met(x,y,z))/dt
+           dmkw_dt(x,y,z) = (newmkw(x,y,z) - mkw(x,y,z))/dt
+           dpht_dt(x,y,z) = (newpht(x,y,z) - pht(x,y,z))/dt
            dpyr_dt(x,y,z) = (newpyr(x,y,z) - pyr(x,y,z))/dt
+           denv_dt(x,y,z) = (newenv(x,y,z) - env(x,y,z))/dt
         end do
      end do
   end do
@@ -293,10 +363,11 @@ subroutine pfsolve(iter)
   do x = 1,psx
      do y = 1,psy
         do z = 2,psz+1
-           pht(x,y,z) = newpht(x,y,z)
-           env(x,y,z) = newenv(x,y,z)
            met(x,y,z) = newmet(x,y,z)
+           mkw(x,y,z) = newmkw(x,y,z)
+           pht(x,y,z) = newpht(x,y,z)
            pyr(x,y,z) = newpyr(x,y,z)
+           env(x,y,z) = newenv(x,y,z)
         end do
      end do
   end do
