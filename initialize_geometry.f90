ccf55dc3e422abf3d153c3a602503c66ec50aef6
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-03-13 15:58:20 -0400

    Simplified inital system configuration. Now the bare metal is in direct contact with the environment. All film phase fields are time-evolved from this initial configuration.

--- initialize_geometry.f90
+++ initialize_geometry.f90
@@ -7,11 +7,10 @@ subroutine initialize_geometry()
   implicit none
 
   integer :: x,y,z
-  integer :: pht_z_beg, pht_z_end
+  integer :: met_z_end
   integer :: rank_loop
 
-  pht_z_beg = 7*(psz_g/16)
-  pht_z_end = 15*(psz_g/16)
+  met_z_end = 15*(psz_g/16)
 
   avg_mu_met = mus_met_mkw_eqb - (R*T*1.5d0) 
   avg_mu_mkw = mus_mkw_pht_eqb - (R*T*0.1d0) 
@@ -25,15 +24,11 @@ subroutine initialize_geometry()
   do x = 1,psx_g
      do y = 1,psy_g
 
-        do z = 1,pht_z_beg
+        do z = 1,met_z_end
            met_g(x,y,z) = 1.0d0; mkw_g(x,y,z) = 0.0d0; pht_g(x,y,z) = 0.0d0; pyr_g(x,y,z) = 0.0d0; env_g(x,y,z) = 0.0d0
         end do
 
-        do z = pht_z_beg+1, pht_z_end
-           met_g(x,y,z) = 0.0d0; mkw_g(x,y,z) = 0.0d0; pht_g(x,y,z) = 1.0d0; pyr_g(x,y,z) = 0.0d0; env_g(x,y,z) = 0.0d0
-        end do
-
-        do z = pht_z_end+1,psz_g
+        do z = met_z_end+1,psz_g
            met_g(x,y,z) = 0.0d0; mkw_g(x,y,z) = 0.0d0; pht_g(x,y,z) = 0.0d0; pyr_g(x,y,z) = 0.0d0; env_g(x,y,z) = 1.0d0
         end do
 
@@ -46,22 +41,14 @@ subroutine initialize_geometry()
   do x = 1,psx_g
      do y = 1,psy_g
 
-        do z = 1,pht_z_beg
+        do z = 1,met_z_end
            mu_g(x,y,z) = avg_mu_met
         end do
 
-        do z = pht_z_beg+1, pht_z_end
-           mu_g(x,y,z) = avg_mu_met + (z-pht_z_beg)*150
-        end do
-
-        do z = pht_z_end+1,psz_g
+        do z = met_z_end+1,psz_g
            mu_g(x,y,z) = avg_mu_env
         end do
 
-        do z = -2,2
-           mu_g(x,y,pht_z_beg+z) = mu_g(x,y,pht_z_beg-3)+((mu_g(x,y,pht_z_beg+3)-mu_g(x,y,pht_z_beg-3))*(0.2*(z+2)))
-        end do
-        
      end do
   end do
 
