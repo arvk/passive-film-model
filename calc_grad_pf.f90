48e790e88d903fc618f25a1c94608e15be9113fe
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-06-04 12:17:42 -0400

    Edited for readability

--- calc_grad_pf.f90
+++ calc_grad_pf.f90
@@ -4,29 +4,27 @@ subroutine calc_grad_pf
   use gradients
   implicit none
 
-  integer :: x, y, z   ! Loop variables
-  integer :: wrap
+  integer :: x, y, z ! Index for x-, y- and z-direction (Loop)
+  integer :: wrap    ! Wrap index along the x-, y-, and z-direction
 
-!!!! Initialize all variables to zero
-  delypyr = 0.0d0
-  delzpyr = 0.0d0
+!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
 
-!!!!! Calculating y-gradient in the bulk
+  delypyr = 0.0d0 ! Initialize y-gradient to zero
+  delzpyr = 0.0d0 ! Initialize z-gradient to zero
 
   do x = 1,psx
      do y = 1,psy
         do z = 1,psz+(2*ghost_width)
-           delypyr(x,y,z) = (pyr(x,wrap(y+1,psy),z)-pyr(x,wrap(y-1,psy),z))/2.0d0
+           delypyr(x,y,z) = (pyr(x,wrap(y+1,psy),z)-pyr(x,wrap(y-1,psy),z))/2.0d0   ! Calculating y-gradient in the bulk
         end do
      end do
   end do
 
-!!!!! Calculating z-gradient in the bulk
   do x = 1,psx
      do y = 1,psy
         delzpyr(x,y,1) = (pyr(x,y,2)-pyr(x,y,1))
         do z = 2,psz+(2*ghost_width)-2
-           delzpyr(x,y,z) = (pyr(x,y,z+1)-pyr(x,y,z-1))/2.0d0
+           delzpyr(x,y,z) = (pyr(x,y,z+1)-pyr(x,y,z-1))/2.0d0                       ! Calculating z-gradient in the bulk
         end do
         delzpyr(x,y,psz+(2*ghost_width)) = (pyr(x,y,psz+(2*ghost_width))-pyr(x,y,psz+(2*ghost_width)-1))
      end do
