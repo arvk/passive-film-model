f9fe8798f67ef97246e194293b0c93dd7759bbb2
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-08-23 20:35:51 -0400

    One-way-coupled PF and film-env kMC

--- spparks_filmenv.f90
+++ spparks_filmenv.f90
@@ -12,6 +12,10 @@ subroutine spparks_filmenv()
   character*1 , target :: myargv
   type(c_ptr), target :: myspparks
 
+  integer, dimension(psx,psy) :: interface_loc
+  real*8, dimension(psx,psy) :: vac_form_bias
+  integer :: x,y,z,xfine,yfine
+
   interface
 
      subroutine spparks_close(instance) bind(C,NAME='spparks_close')
@@ -50,12 +54,56 @@ subroutine spparks_filmenv()
 
   end interface
 
-
   myargc = 0
   myargv = C_NULL_CHAR
 
+  call gather_pf()
+
+  if(isroot)then
+     interface_loc = 0
+
+     do x = 1,psx
+        do y = 1,psy
+           do z = 1,psz_g
+              if ((env_g(x,y,z) .lt. 5.0E-1).and.(env_g(x,y,z+1) .gt. 5.0E-1)) then
+                 interface_loc(x,y) = z
+                 vac_form_bias(x,y) = sin(x*y/5.0d0)+1.0d0
+                 exit
+              end if
+           end do
+        end do
+     end do
+
+     call system('rm input.filmenv')
+
+     open(unit = 666, file = 'input.filmenv', status = 'new')
+     write(666,*) 'Testing'
+     write(666,*) '2 dimension'
+     write(666,*) '0 ', psx*kg_scale ,' xlo xhi'
+     write(666,*) '0 ', psy*kg_scale ,' ylo yhi'
+     write(666,*) '-0.5 0.5 zlo zhi'
+     write(666,*) psx*psy*kg_scale*kg_scale, ' sites'
+     write(666,*) ' '
+     write(666,*) 'Values'
+     write(666,*) ' '
+
+     do x = 0,psx-1
+        do y = 0,psy-1
+           do xfine = 0,kg_scale-1
+              do yfine = 0,kg_scale-1
+                 write(666,'(I8,I3,I7,F9.6)') 1+((x*kg_scale)+xfine)+(((y*kg_scale)+yfine)*(kg_scale*psx)), 0, interface_loc(x+1,y+1), vac_form_bias(x+1,y+1)
+              end do
+           end do
+        end do
+     end do
+
+     close(666)
+
+  end if
+
+  call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
+
   call spparks_open(myargc,C_LOC(myargv),C_LOC(MPI_COMM_WORLD),C_LOC(myspparks))
-  write(6,*) 'Read properly'
   call spparks_file(myspparks,'filmenv.spparksscript'//C_NULL_CHAR)
   call spparks_close(myspparks)
 
