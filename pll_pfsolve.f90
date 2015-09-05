27e53f179d9139774cc9d82cc2f6705bcec01160
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-05 18:50:26 -0400

    Added 1/t term to the PF jacobian and removed comments

--- pll_pfsolve.f90
+++ pll_pfsolve.f90
@@ -37,20 +37,18 @@ subroutine para_pfsolve(iter,snes_pf)
   call VecCopy(state,state_unknown,ierr)
   call DMRestoreGlobalVector(da,state,ierr)
 
-  call MatCreate(MPI_COMM_WORLD,mat_jacob,ierr)
-  call MatSetSizes(mat_jacob,PETSC_DECIDE, PETSC_DECIDE,psx_g*psy_g*psz_g*nfields,psx_g*psy_g*psz_g*nfields,ierr)
+  call DMCreateMatrix(da,mat_jacob,ierr)
+  call MatSeqAIJSetPreallocation(mat_jacob,7*5,PETSC_NULL_INTEGER,ierr)  !!! ADD MatMPIAIJSetPreallocation
 
   call SNESSetFunction(snes_pf,vec_feval,FormFunction_pf,ctx,ierr)
   call SNESSetJacobian(snes_pf,mat_jacob,mat_jacob,FormJacobian_pf,ctx,ierr)
   call FormRHS_pf(state_unknown,rhs_vec)
 
-  call VecView(state_unknown,PETSC_NULL_OBJECT,ierr)
-  write(6,*) "DIFFERENCE"
-  call VecView(rhs_vec,PETSC_NULL_OBJECT,ierr)
 
-!  call SNESSolve(snes_pf,rhs_vec,state_unknown,ierr)
+  call SNESSolve(snes_pf,rhs_vec,state_unknown,ierr)
 
-
+!  call VecAXPY(state_unknown,-1.0d0,state,ierr)
+!  call VecView(state_unknown,PETSC_NULL_OBJECT,ierr)
 
 
 end subroutine para_pfsolve
@@ -99,8 +97,6 @@ subroutine FormRHS_pf(input_state,rhs_vec)
   call VecSetSizes(single_phase_vector,PETSC_DECIDE,psx_g*psy_g*psz_g,ierr)
   call VecSetUp(single_phase_vector,ierr)
 
-!  call SNESGetDM(snes_pf,da,ierr)
-!  call DMGetGlobalVector(da,state,ierr)
 
   call VecDuplicate(input_state,rhs_vec,ierr)
 
@@ -110,11 +106,8 @@ subroutine FormRHS_pf(input_state,rhs_vec)
      call VecStrideScatter(single_phase_vector,fesphase,rhs_vec,INSERT_VALUES,ierr)
   end do
 
-!  call DMRestoreGlobalVector(da,state,ierr)
-
   call VecAssemblyBegin(rhs_vec,ierr)
   call VecAssemblyEnd(rhs_vec,ierr)
-
   return
 end subroutine FormRHS_pf
 
@@ -178,25 +171,15 @@ subroutine FormFunction_pf(snes_pf,input_state,function_value,ctx,ierr)
   real*8 :: w(0:nfields), delo(0:nfields)
 
 
-!  call VecView(input_state,PETSC_NULL_OBJECT,ierr)
-
   w = 0.0d0
   delo = 0.0d0
 
-!  write(6,*) 'Got into form function'
   call SNESGetDM(snes_pf,da,ierr)
-!  write(6,*) '1'
   call DMGetLocalVector(da,state_local,ierr)
-!  write(6,*) '2'
   call DMGlobalToLocalBegin(da,input_state,INSERT_VALUES,state_local,ierr)
-!  write(6,*) '3'
   call DMGlobalToLocalEnd(da,input_state,INSERT_VALUES,state_local,ierr)
 
-!  write(6,*) 'Extracted DA and copied to local array'
-
   call DMDAGetCorners(da,startx,starty,startz,widthx,widthy,widthz,ierr)
-
-!  write(6,*) 'Extracted local domain bounds'
 
   call VecDuplicate(input_state,function_value,ierr)
   call VecSetUp(function_value,ierr)
@@ -204,7 +187,6 @@ subroutine FormFunction_pf(snes_pf,input_state,function_value,ctx,ierr)
   call DMDAVecGetArrayF90(da,state_local,statepointer,ierr)
   call DMDAVecGetArrayF90(da,function_value,functionpointer,ierr)
 
-!  write(6,*) 'Extracted All arrays'
 
   do z=startz,startz+widthz-1
      do y=starty,starty+widthy-1
@@ -269,20 +251,12 @@ subroutine FormFunction_pf(snes_pf,input_state,function_value,ctx,ierr)
      end do
   end do
 
-!  write(6,*) 'Wrote all arrays'
 
   call DMDAVecRestoreArrayF90(da,state_local,statepointer,ierr)
   call DMDAVecRestoreArrayF90(da,function_value,functionpointer,ierr)
 
-!  write(6,*) 'Restored all arrays'
-
   call VecAssemblyBegin(function_value,ierr)
   call VecAssemblyEnd(function_value,ierr)
-
-!  call VecView(function_value,PETSC_NULL_OBJECT,ierr)
-
-!  write(6,*) 'Done with formfunction'
-
   return
 end subroutine FormFunction_pf
 
@@ -320,28 +294,20 @@ subroutine FormJacobian_pf(snes_pf,input_state,pf_jacob,pf_precond,ctx,ierr)
   real*8 :: Mobility, hill, S
   real*8 :: w(0:nfields), delo(0:nfields)
   PetscScalar  v((7*2*(nfields-1))+2)
-  MatStencil   row(4,1),col(4,(7*2*(nfields-1))+2)
+  MatStencil   row(4,1),col(4,(7*2*(nfields-1))+3)
   integer :: nocols
   Mat pf_jacob, pf_precond
 
   w = 0.0d0
   delo = 0.0d0
 
+
   call SNESGetDM(snes_pf,da,ierr)
   call DMGetLocalVector(da,state_local,ierr)
-  call DMGlobalToLocalBegin(da,input_state,INSERT_VALUES,state_local)
-  call DMGlobalToLocalEnd(da,input_state,INSERT_VALUES,state_local)
-
-
+  call DMGlobalToLocalBegin(da,input_state,INSERT_VALUES,state_local,ierr)
+  call DMGlobalToLocalEnd(da,input_state,INSERT_VALUES,state_local,ierr)
   call DMDAGetCorners(da,startx,starty,startz,widthx,widthy,widthz,ierr)
   call DMDAVecGetArrayF90(da,state_local,statepointer,ierr)
-
-
-
-
-  write(6,*) 'In FormJacobian_pf'
-
-
 
 
   do z=startz,startz+widthz-1
@@ -515,6 +481,14 @@ subroutine FormJacobian_pf(snes_pf,input_state,pf_jacob,pf_precond,ctx,ierr)
                      col(MatStencil_k,nocols) = z
                      col(MatStencil_c,nocols) = fesphase
 
+
+                     nocols = nocols + 1
+                     v(nocols) = 1.0d0/dt
+                     col(MatStencil_i,nocols) = x
+                     col(MatStencil_j,nocols) = y
+                     col(MatStencil_k,nocols) = z
+                     col(MatStencil_c,nocols) = fesphase
+
                      call MatSetValuesStencil(pf_precond,1,row,nocols,col,v,ADD_VALUES,ierr)
 
            end do
@@ -527,9 +501,6 @@ subroutine FormJacobian_pf(snes_pf,input_state,pf_jacob,pf_precond,ctx,ierr)
 
   call MatAssemblyBegin(pf_precond,MAT_FINAL_ASSEMBLY,ierr)
   call MatAssemblyEnd(pf_precond,MAT_FINAL_ASSEMBLY,ierr)
-
-  call MatView(pf_precond,PETSC_NULL_OBJECT,ierr)
-
   return
 end subroutine FormJacobian_pf
 
