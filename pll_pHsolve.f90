69c0e63a428d4687dd965d454fd6f0be7ffd0867
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-07 20:18:42 -0400

    pHsolver uses user context now

--- pll_pHsolve.f90
+++ pll_pHsolve.f90
@@ -1,4 +1,4 @@
-subroutine para_pHsolve(iter,ksp_pH)
+subroutine para_pHsolve(iter,ksp_pH,simstate)
   use commondata
   use fields
   use thermo_constants
@@ -19,21 +19,18 @@ subroutine para_pHsolve(iter,ksp_pH)
   PetscErrorCode ierr
   KSP ksp_pH
   KSPConvergedReason pH_converged_reason
-  DM da
   Vec solved_pH_vector
   Vec state,state_solved
   PetscInt ctx
+  integer, intent(in) :: iter  ! Iteration count
+  integer :: x, y, z           ! Index for x-, y-, and z-direction (Loop)
+  type(context) simstate
   external computeRHS_pH, computeMatrix_pH, computeInitialGuess_pH
 
 
-  integer, intent(in) :: iter  ! Iteration count
-  integer :: x, y, z           ! Index for x-, y-, and z-direction (Loop)
-
-  call KSPGetDM(ksp_pH,da,ierr)
-
-  call KSPSetComputeRHS(ksp_pH,computeRHS_pH,ctx,ierr)
-  call KSPSetComputeOperators(ksp_pH,computeMatrix_pH,ctx,ierr)
-  call KSPSetComputeInitialGuess(ksp_pH,computeInitialGuess_pH,ctx,ierr)
+  call KSPSetComputeRHS(ksp_pH,computeRHS_pH,simstate,ierr)
+  call KSPSetComputeOperators(ksp_pH,computeMatrix_pH,simstate,ierr)
+  call KSPSetComputeInitialGuess(ksp_pH,computeInitialGuess_pH,simstate,ierr)
 
   call KSPSolve(ksp_pH,PETSC_NULL_OBJECT,PETSC_NULL_OBJECT,ierr)
   call KSPGetSolution(ksp_pH,state_solved,ierr)
@@ -43,9 +40,9 @@ subroutine para_pHsolve(iter,ksp_pH)
   call VecSetUp(solved_pH_vector,ierr)
   call VecStrideGather(state_solved,npH,solved_pH_vector,INSERT_VALUES,ierr)
 
-  call DMGetGlobalVector(da,state,ierr)
+  call DMGetGlobalVector(simstate%lattval,state,ierr)
   call VecStrideScatter(solved_pH_vector,npH,state,INSERT_VALUES,ierr)
-  call DMRestoreGlobalVector(da,state,ierr)
+  call DMRestoreGlobalVector(simstate%lattval,state,ierr)
 
 end subroutine para_pHsolve
 
@@ -62,7 +59,7 @@ end subroutine para_pHsolve
 
 
 
-subroutine computeInitialGuess_pH(ksp_pH,b,ctx,ierr)
+subroutine computeInitialGuess_pH(ksp_pH,b,simstate,ierr)
   use commondata
   use fields
   use thermo_constants
@@ -81,19 +78,16 @@ subroutine computeInitialGuess_pH(ksp_pH,b,ctx,ierr)
   KSP ksp_pH
   PetscInt ctx
   PetscErrorCode ierr
-  DM da
   Vec state, b, onlypH
+  type(context) simstate
 
-  call KSPGetDM(ksp_pH,da,ierr)
-
-  call DMGetGlobalVector(da,state,ierr)
+  call DMGetGlobalVector(simstate%lattval,state,ierr)
   call VecDuplicate(state,b,ierr)
   call VecCopy(state,b,ierr)
-  call DMRestoreGlobalVector(da,state,ierr)
+  call DMRestoreGlobalVector(simstate%lattval,state,ierr)
 
   call VecAssemblyBegin(b,ierr)
   call VecAssemblyEnd(b,ierr)
-
   return
 end subroutine computeInitialGuess_pH
 
@@ -112,7 +106,7 @@ end subroutine computeInitialGuess_pH
 
 
 
-subroutine computeRHS_pH(ksp_pH,b,ctx,ierr)
+subroutine computeRHS_pH(ksp_pH,b,simstate,ierr)
   use commondata
   use fields
   use thermo_constants
@@ -132,15 +126,12 @@ subroutine computeRHS_pH(ksp_pH,b,ctx,ierr)
   KSP ksp_pH
   PetscInt ctx
   PetscErrorCode ierr
-  DM da
   Vec state, onlyenv, onlypH, b
   PetscScalar, pointer :: pHpointer(:), envpointer(:)
-  integer :: i,j,k,startx,starty,startz,widthx,widthy,widthz
+  integer :: i,j,k
+  type(context) simstate
 
-  call KSPGetDM(ksp_pH,da,ierr)
-  call DMDAGetCorners(da,startx,starty,startz,widthx,widthy,widthz,ierr)
-
-  call DMGetGlobalVector(da,state,ierr)
+  call DMGetGlobalVector(simstate%lattval,state,ierr)
   call VecCreate(MPI_COMM_WORLD,onlyenv,ierr)
   call VecSetSizes(onlyenv,PETSC_DECIDE,psx_g*psy_g*psz_g,ierr)
   call VecSetUp(onlyenv,ierr)
@@ -150,13 +141,13 @@ subroutine computeRHS_pH(ksp_pH,b,ctx,ierr)
   call VecSetUp(onlypH,ierr)
   call VecStrideGather(state,npH,onlypH,INSERT_VALUES,ierr)
   call VecDuplicate(state,b,ierr)
-  call DMRestoreGlobalVector(da,state,ierr)
+  call DMRestoreGlobalVector(simstate%lattval,state,ierr)
 
 
   call VecGetArrayF90(onlypH,pHpointer,ierr)
   call VecGetArrayF90(onlyenv,envpointer,ierr)
 
-  do i=1,(widthx*widthy*widthz)
+  do i=1,(simstate%widthx*simstate%widthy*simstate%widthz)
      if(envpointer(i).gt.0.97d0) then
         pHpointer(i) = pHpointer(i)/dt
      else
@@ -171,7 +162,6 @@ subroutine computeRHS_pH(ksp_pH,b,ctx,ierr)
 
   call VecAssemblyBegin(b,ierr)
   call VecAssemblyEnd(b,ierr)
-
   return
 end subroutine computeRHS_pH
 
@@ -180,7 +170,7 @@ end subroutine computeRHS_pH
 
 
 
-subroutine ComputeMatrix_pH(ksp_pH,matoper,matprecond,ctx,ierr)
+subroutine ComputeMatrix_pH(ksp_pH,matoper,matprecond,simstate,ierr)
   use commondata
   use fields
   use thermo_constants
@@ -199,38 +189,30 @@ subroutine ComputeMatrix_pH(ksp_pH,matoper,matprecond,ctx,ierr)
   KSP ksp_pH
   PetscInt ctx
   PetscErrorCode ierr
-  DM da
   Vec state,statelocal
   Mat matoper, matprecond
   PetscInt     i,j,k
   PetscScalar  v(7)
   MatStencil   row(4,1),col(4,7)
   PetscScalar, pointer :: statepointer(:,:,:,:)
-  integer :: startx,starty,startz,widthx,widthy,widthz
+  type(context) simstate
   real*8 :: D
   integer :: nocols, nox, noy, noz
   real*8 :: add_to_v_ij
 
-  write(6,*) "In mat comp", rank
-
   D = D_H_env
 
-  call KSPGetDM(ksp_pH,da,ierr)
+  call DMGetGlobalVector(simstate%lattval,state,ierr)
+  call DMCreateLocalVector(simstate%lattval,statelocal,ierr)
+  call DMGlobalToLocalBegin(simstate%lattval,state,INSERT_VALUES,statelocal,ierr)
+  call DMGlobalToLocalEnd(simstate%lattval,state,INSERT_VALUES,statelocal,ierr)
+  call DMRestoreGlobalVector(simstate%lattval,state,ierr)
 
-  call DMGetGlobalVector(da,state,ierr)
-  call DMCreateLocalVector(da,statelocal,ierr)
-  call DMGlobalToLocalBegin(da,state,INSERT_VALUES,statelocal,ierr)
-  call DMGlobalToLocalEnd(da,state,INSERT_VALUES,statelocal,ierr)
-  call DMRestoreGlobalVector(da,state,ierr)
+  call DMDAVecGetArrayF90(simstate%lattval,statelocal,statepointer,ierr)
 
-
-  call DMDAVecGetArrayF90(da,statelocal,statepointer,ierr)
-  call DMDAGetCorners(da,startx,starty,startz,widthx,widthy,widthz,ierr)
-
-
-  do k=startz,startz+widthz-1
-     do j=starty,starty+widthy-1
-        do i=startx,startx+widthx-1
+  do k=simstate%startz,simstate%startz+simstate%widthz-1
+     do j=simstate%starty,simstate%starty+simstate%widthy-1
+        do i=simstate%startx,simstate%startx+simstate%widthx-1
 
            row(MatStencil_i,1) = i
            row(MatStencil_j,1) = j
@@ -335,7 +317,7 @@ subroutine ComputeMatrix_pH(ksp_pH,matoper,matprecond,ctx,ierr)
      end do
   end do
 
-  call DMDAVecRestoreArrayF90(da,statelocal,statepointer,ierr)
+  call DMDAVecRestoreArrayF90(simstate%lattval,statelocal,statepointer,ierr)
 
   call MatAssemblyBegin(matprecond,MAT_FINAL_ASSEMBLY,ierr)
   call MatAssemblyEnd(matprecond,MAT_FINAL_ASSEMBLY,ierr)
