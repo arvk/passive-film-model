2f72e5e78b8730559bc84aff24c9233bd70666a6
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-19 15:21:32 -0400

    Metal-film kMC compatible with the parallel solver routines

--- spparks_metfilm.f90
+++ spparks_metfilm.f90
@@ -1,29 +1,34 @@
-subroutine spparks_metfilm()
+subroutine spparks_metfilm(iter,simstate)
   use, intrinsic :: iso_c_binding
   use commondata
   use fields
   implicit none
-  include 'mpif.h'
+#include <finclude/petscsys.h>
+#include <finclude/petscvec.h>
+#include <finclude/petscmat.h>
+#include <finclude/petscpc.h>
+#include <finclude/petscksp.h>
+#include <finclude/petscsnes.h>
+#include <finclude/petscdm.h>
+#include <finclude/petscdmda.h>
+#include <finclude/petscdmda.h90>
 
   integer :: ierr, my_rank
-
   integer (C_INT) :: myargc
   character*1 , target :: myargv
   type(c_ptr), target :: myspparks
-
-  integer, dimension(psx,psy) :: interface_loc
-  real*8, dimension(psx,psy) :: distance_interface_moved
-  real*8, dimension(psx,psy) :: vac_form_bias
+  integer, dimension(psx_g,psy_g) :: interface_loc
+  real*8, dimension(psx_g,psy_g) :: distance_interface_moved
+  real*8, dimension(psx_g,psy_g) :: vac_form_bias
   integer :: x,y,z,xfine,yfine
   integer :: nint
-
-  integer,dimension(psx*kg_scale*psy*kg_scale*4) :: site_id, partial_x, partial_y, i1, i2, no_h2_already_evolved
-
+  integer :: site_id, partial_x, partial_y, i1, i2, no_h2_already_evolved
   character*24 :: kmc_numel_string
-  integer, dimension(psx*kg_scale,psy*kg_scale) :: fine_kmc_array
+  integer, dimension(psx_g*kg_scale,psy_g*kg_scale) :: fine_kmc_array
   real*8 :: average_from_fine
-
   logical :: already_run_once
+  type(context) simstate
+  integer, intent(in) :: iter  ! Iteration count
 
   interface
 
@@ -68,77 +73,59 @@ subroutine spparks_metfilm()
 
   INQUIRE(FILE='input.metfilm', EXIST=already_run_once)
 
-  call gather_pf()
+  call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
 
   if(isroot)then
-
      call system('cp metfilm.spparksscript.template metfilm.spparksscript')
-
-     if (already_run_once) then
-        call system('echo "read_sites input.metfilm" >> metfilm.spparksscript')
-     end if
-
-     call system('echo "run 10000" >> metfilm.spparksscript')
-
+     if (already_run_once) call system('echo "read_sites input.metfilm" >> metfilm.spparksscript')
+     call system('echo "run 100" >> metfilm.spparksscript')
   end if
-
-
-
   call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
 
   call spparks_open(myargc,C_LOC(myargv),C_LOC(MPI_COMM_WORLD),C_LOC(myspparks))
   call spparks_file(myspparks,'metfilm.spparksscript'//C_NULL_CHAR)
   call spparks_close(myspparks)
 
-
-
-
-
-
+  call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
 
   if(isroot)then
-
      call system('rm -f input.metfilm log.spparks')
-
      ! READ RAW SPPARKS OUTPUT AND WRITE FORTRAN-READABLE OUTPUT
-     write(kmc_numel_string,'(I24)') psx*psy*kg_scale*kg_scale*4
+     write(kmc_numel_string,'(I24)') psx_g*psy_g*kg_scale*kg_scale*4
      call system('tail -n '//trim(kmc_numel_string)//' raw_metfilm_output | awk ''{printf " %7.7i %4.4i %4.4i %3.3i %3.3i %5.5i \n", $1,$2,$3,$4,$5,$6}'' > metfilm_spparks_output.template')
      call system('rm -f raw_metfilm_output')
+  end if
 
-     ! READ FORTRAN-READABLE INPUT AND STORE IN ARRAY FOR COUPLING
-     open (unit = 667, file = 'metfilm_spparks_output.template', status = 'old')
-     do x = 1,psx*psy*kg_scale*kg_scale*4
-        read(667,'(I8, I5, I5, I4, I4, I6)') site_id(x), partial_x(x), partial_y(x), i1(x), i2(x), no_h2_already_evolved(x)
-     end do
-     close(667)
-     call system('rm -f metfilm_spparks_output.template')
+  call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
 
-     ! WRITE SPPARKS-READABLE OUTPUT
+  if(isroot)then
+     !  READ FORTRAN-READABLE INPUT AND STORE IN ARRAY FOR COUPLING
+
      call system('rm -f input.metfilm')
      open(unit = 666, file = 'input.metfilm', status = 'new')
+     open (unit = 667, file = 'metfilm_spparks_output.template', status = 'old')
+
      write(666,*) 'Testing'
      write(666,*) '2 dimension'
-     write(666,*) '0 ', psx*kg_scale ,' xlo xhi'
-     write(666,*) '0 ', psy*kg_scale ,' ylo yhi'
+     write(666,*) '0 ', psx_g*kg_scale ,' xlo xhi'
+     write(666,*) '0 ', psy_g*kg_scale ,' ylo yhi'
      write(666,*) '-0.5 0.5 zlo zhi'
-     write(666,*) psx*psy*kg_scale*kg_scale*4, ' sites'
+     write(666,*) psx_g*psy_g*kg_scale*kg_scale*4, ' sites'
      write(666,*) ' '
      write(666,*) 'Values'
      write(666,*) ' '
 
-     do x = 1,psx*psy*kg_scale*kg_scale*4
-        write(666,'(I8,I4,I4,I6)') site_id(x), i1(x), i2(x), no_h2_already_evolved(x)
+     do x = 1,psx_g*psy_g*kg_scale*kg_scale*4
+        read(667,'(I8, I5, I5, I4, I4, I6)') site_id, partial_x, partial_y, i1, i2, no_h2_already_evolved
+        write(666,'(I8,I4,I4,I6)') site_id, i1, i2, no_h2_already_evolved
      end do
 
+     close(667)
      close(666)
-
-
-     ! do x = 1,psx*psy*kg_scale*kg_scale*4
-     !    fine_kmc_array(partial_x(x)+1,partial_y(x)+1) = fine_kmc_array(partial_x(x),partial_y(x)) + no_h2_already_evolved(x)
-     ! end do
-
+     call system('rm -f metfilm_spparks_output.template')
 
   end if
 
+  call mpi_barrier(MPI_COMM_WORLD,ierr) ! Barrier before beginning SPPARKS functions
 
 end subroutine spparks_metfilm
