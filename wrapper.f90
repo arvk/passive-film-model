8962378e442da4d9e3d114fc5bb6ce3ddebae1e2
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-25 11:12:39 -0400

    Linear and Nonlinear solvers are created and destroyed at each timestep

--- wrapper.f90
+++ wrapper.f90
@@ -49,18 +49,6 @@ program passive_film_model
   call DMCreateGlobalVector(simstate%lattval, simstate%exslice, ierr)
 
 
-  call KSPCreate(MPI_COMM_WORLD,ksp_mu,ierr)
-  call KSPCreate(MPI_COMM_WORLD,ksp_pH,ierr)
-  call KSPCreate(MPI_COMM_WORLD,ksp_ang,ierr)
-  call SNESCreate(MPI_COMM_WORLD,snes_pf,ierr)
-  call SNESCreate(MPI_COMM_WORLD,snes_pot,ierr)
-
-
-
-
-
-
-
 
   call allocate_matrices() ! Allocate all field matrices
   call diffusivities()     ! Calculate phase diffusivities
@@ -108,11 +96,11 @@ program passive_film_model
      call VecCopy(simstate%slice,simstate%exslice,ierr)
 
      write(6,*) 'In iteration',iter
-     call para_pfsolve(iter,snes_pf,simstate)
-     call para_musolve(iter,ksp_mu,simstate)
-     call para_pHsolve(iter,ksp_pH,simstate)
-     call para_angsolve(iter,ksp_ang,simstate)
-     call para_potsolve(iter,snes_pot,simstate)
+     call SNESCreate(MPI_COMM_WORLD,snes_pf,ierr); call para_pfsolve(iter,snes_pf,simstate); call SNESDestroy(snes_pf,ierr)
+     call KSPCreate(MPI_COMM_WORLD,ksp_mu,ierr); call para_musolve(iter,ksp_mu,simstate); call KSPDestroy(ksp_mu,ierr)
+     call KSPCreate(MPI_COMM_WORLD,ksp_pH,ierr); call para_pHsolve(iter,ksp_pH,simstate); call KSPDestroy(ksp_pH,ierr)
+     call KSPCreate(MPI_COMM_WORLD,ksp_ang,ierr); call para_angsolve(iter,ksp_ang,simstate); call KSPDestroy(ksp_ang,ierr)
+     call SNESCreate(MPI_COMM_WORLD,snes_pot,ierr); call para_potsolve(iter,snes_pot,simstate); call SNESDestroy(snes_pot,ierr)
 
      call spparks_filmenv(iter,simstate)
      call spparks_metfilm(iter,simstate)
@@ -125,15 +113,8 @@ program passive_film_model
 !!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
 
   !! Destroy Unused objects
-  call KSPDestroy(ksp_mu,ierr)
-  call KSPDestroy(ksp_pH,ierr)
-  call KSPDestroy(ksp_ang,ierr)
-  call SNESDestroy(snes_pf,ierr)
-  call SNESDestroy(snes_pot,ierr)
-
   call VecDestroy(simstate%slice,ierr)
   call VecDestroy(simstate%exslice,ierr)
-
   call DMDestroy(simstate%lattval,ierr)
 
   !! Finalize Parallelization
