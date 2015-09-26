8fd06d0cdd9b846e045c46ee10e7463be4d86b1d
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-26 12:24:09 -0400

    Ford documentation for the timestep estimation routine

--- estimate_timestep.f90
+++ estimate_timestep.f90
@@ -3,16 +3,17 @@ subroutine estimate_timestep
   use commondata
   use diffusion_constants
   implicit none
+  !! **Estimate a stable timestep for phase field and chemical potential field evolution**
 
-  real*8 :: dt_stable_diffusion    ! Maximum stable forward-euler timestep for integrating the mu field
-  real*8 :: dt_stable_phase_field  ! Maximum stable forward-euler timestep for integrating the phase field
-  real*8 :: max_M_sigma            ! Maximum of Mobility*sigma for all the phases
+  real*8 :: dt_stable_diffusion    !! Maximum stable forward-euler timestep for integrating the chemical-potential field
+  real*8 :: dt_stable_phase_field  !! Maximum stable forward-euler timestep for integrating the phase field
+  real*8 :: max_M_sigma            !! Maximum of Mobility*sigma for all the phases
 
-!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
+!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
   dt_stable_diffusion = (dpf*dpf)/(2*max(D_S_pht, D_S_pyr, D_S_mkw, D_Fe_pht, D_Fe_pyr, D_Fe_mkw)) ! Standard Forward-Euler criterion
 
-!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
+!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
   max_M_sigma = max(M_met_mkw*sigma_met_mkw,M_met_pht*sigma_met_pht,M_met_pyr*sigma_met_pyr_0,M_met_env*sigma_met_env)
   max_M_sigma = max(max_M_sigma,M_mkw_pht*sigma_mkw_pht,M_mkw_pyr*sigma_mkw_pyr_0,M_mkw_env*sigma_mkw_env)
@@ -21,7 +22,7 @@ subroutine estimate_timestep
 
   dt_stable_phase_field = (dpf*dpf)/max_M_sigma ! The prefactor before the laplacian for PF evolution is the sum of M*sigma for each phase.
 
-!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
+!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
   dt = min(dt_stable_phase_field,dt_stable_diffusion)/0.75d0
 
