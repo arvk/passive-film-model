066d54d6023f6d131ef9953f7edabaa86552c969
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-26 12:42:53 -0400

    Ford documentation for the field distribution routine

--- distrib_pf.f90
+++ distrib_pf.f90
@@ -3,34 +3,35 @@ subroutine distrib_pf()
   use fields
   implicit none
   include 'mpif.h'
+  !! **Distribute field variables -- different \(FeS\) phase fractions, chemical potential etc. to child processors**
 
-  integer :: x, y, z   ! Loop variables
-  integer :: ierr,status(MPI_STATUS_SIZE)
+  integer :: x, y, z                      !! Coordinates inside the simulation system
+  integer :: ierr,status(MPI_STATUS_SIZE) !! MPI error and status flags
 
-!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
+!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Broadcast global phase-fraction fields
+  ! Broadcast global phase-fraction fields
   call mpi_bcast(met_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(mkw_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(pht_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(pyr_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
   call mpi_bcast(env_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-  !! Broadcast global chemical-potential fields
+  ! Broadcast global chemical-potential fields
   call mpi_bcast(mu_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-  !! Broadcast global electrical potential
+  ! Broadcast global electrical potential
   call mpi_bcast(elpot_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-  !! Broadcast global pyrite orientation field
+  ! Broadcast global pyrite orientation field
   call mpi_bcast(opyr_g(1,1,1),psx_g*psy_g*psz_g,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-  !! Broadcast the chemical potential of the environment
+  ! Broadcast the chemical potential of the environment
   call mpi_bcast(avg_mu_env,1,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
 
-!!!!!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!!
+!@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Copy relevant portions of the global matrices onto local matrices
+  ! Copy relevant portions of the global matrices onto local matrices
   do x = 1,psx
      do y = 1,psy
         do z = 1,psz
