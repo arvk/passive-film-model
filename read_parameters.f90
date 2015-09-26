5b47ffc635596cfee9f06ca38fcb5a1efddd1498
Aravind Krishnamoorthy <aravindk@mit.edu>
2015-09-26 15:56:23 -0400

    Removed one extra reference to local matrix sizes

--- read_parameters.f90
+++ read_parameters.f90
@@ -12,7 +12,7 @@ subroutine read_parameters()
 
   !!---
   !! Input Tag = RESTART ; Is the simulation being started from scratch?
-  ! Output variable = isrestart 
+  ! Output variable = isrestart
   call system("cat param.in | sed 's/!.*//g' | grep '^ *RESTART' | sed 's/RESTART//g'| sed 's/=//g' > .isrestart.readin")
   open(unit = 7000, file = ".isrestart.readin", status = 'old') ; read(7000,*) isrestart ; close(7000)
   call system ("rm .isrestart.readin")
@@ -27,7 +27,7 @@ subroutine read_parameters()
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
   !! Input Tag = PFSIZE ; What is the size of the phase-field simulation box?
-  ! Output variable = psx_g, psy_g, psz_g 
+  ! Output variable = psx_g, psy_g, psz_g
   call system("cat param.in | sed 's/!.*//g' | grep '^ *PFSIZE' | sed 's/PFSIZE//g'| sed 's/=//g' > .pfsize.readin")
   open(unit = 7101, file = ".pfsize.readin", status = 'old') ; read(7101,*) psx_g, psy_g, psz_g ; close(7101)
   call system ("rm .pfsize.readin")
@@ -36,12 +36,10 @@ subroutine read_parameters()
      stop "ERROR: Phase field grid sizes are non-positive. Exiting."
   end if
 
-  psx = psx_g ; psy = psy_g ; psz = psz_g/procs
-
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag = TEMP ; What is the temperature of the simulation box? 
-  ! Output variable = T 
+  !! Input Tag = TEMP ; What is the temperature of the simulation box?
+  ! Output variable = T
   call system("cat param.in | sed 's/!.*//g' | grep '^ *TEMP' | sed 's/TEMP//g'| sed 's/=//g' > .temp.readin")
   open(unit = 7102, file = ".temp.readin", status = 'old');   read(7102,*,IOSTAT=error_temp) T ; close(7102)
   call system ("rm .temp.readin")
@@ -58,7 +56,7 @@ subroutine read_parameters()
 
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag = PH ; What is the pH of the simulation box? 
+  !! Input Tag = PH ; What is the pH of the simulation box?
   ! Output variable = pH_in
   call system("cat param.in | sed 's/!.*//g' | grep '^ *PH' | sed 's/PH//g'| sed 's/=//g' > .ph.readin")
   open(unit = 7102, file = ".ph.readin", status = 'old');   read(7102,*,IOSTAT=error_ph) pH_in ; close(7102)
@@ -71,7 +69,7 @@ subroutine read_parameters()
 
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag = SIMLEN ; What is the length of the simulation? 
+  !! Input Tag = SIMLEN ; What is the length of the simulation?
   ! Output variable = nomc
   call system("cat param.in | sed 's/!.*//g' | grep '^ *SIMLEN' | sed 's/SIMLEN//g'| sed 's/=//g' > .nomc.readin")
   open(unit = 7103, file = ".nomc.readin", status = 'old');   read(7103,*,IOSTAT=error_nomc) nomc ; close(7103)
@@ -89,7 +87,7 @@ subroutine read_parameters()
 
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag = DISSOLVE ; Should dissolution be included in the simulation? 
+  !! Input Tag = DISSOLVE ; Should dissolution be included in the simulation?
   ! Output variable = include_dissolve
   call system("cat param.in | sed 's/!.*//g' | grep '^ *DISSOLVE' | sed 's/DISSOLVE//g'| sed 's/=//g' > .isdissolve.readin")
   open(unit = 7000, file = ".isdissolve.readin", status = 'old') ; read(7000,*,IOSTAT=error_dissolve) isdissolve ; close(7000)
@@ -114,7 +112,7 @@ subroutine read_parameters()
 
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag= ELECTRO ; Should the electrochemistry module be included? 
+  !! Input Tag= ELECTRO ; Should the electrochemistry module be included?
   ! Output variable = include_electro
   call system("cat param.in | sed 's/!.*//g' | grep '^ *ELECTRO' | sed 's/ELECTRO//g'| sed 's/=//g' > .useelectro.readin")
   open(unit = 7000, file = ".useelectro.readin", status = 'old') ; read(7000,*,IOSTAT=error_electro) useelectro ; close(7000)
@@ -151,7 +149,7 @@ subroutine read_parameters()
 
 !@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!
 
-  !! Input Tag = SNAPSHOTS ; How many output snapshots? 
+  !! Input Tag = SNAPSHOTS ; How many output snapshots?
   ! Output variable = noimg
   call system("cat param.in | sed 's/!.*//g' | grep '^ *SNAPSHOTS' | sed 's/SNAPSHOTS//g'| sed 's/=//g' > .nosnapshots.readin")
   open(unit = 7109, file = ".nosnapshots.readin", status = 'old');   read(7109,*,IOSTAT=error_snapshots) noimg ; close(7109)
